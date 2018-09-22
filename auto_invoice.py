121c28a3e9fadb99a9afe123b502db8f93e0891d
Ian Sutter <stormchaser30000@gmail.com>
2018-09-21 19:32:19 -0500

    make the program print a list of auto services and their prices, as well as the name of the auto shop

---
+++ auto_invoice.py
@@ -0,0 +1,16 @@
+import math
+
+# print the name of the auto shop
+print('Davy\'s auto shop services')
+
+# make and print a list of automotive services
+auto_services = {
+        'Oil change': 35,
+        'Tire rotation': 19,
+        'Car wash': 7,
+        'Car wax': 12,
+}
+print(f"Oil change -- ${auto_services['Oil change']}")
+print(f"Tire rotation -- ${auto_services['Tire rotation']}")
+print(f"Car wash -- ${auto_services['Car wash']}")
+print(f"Car wax -- ${auto_services['Car wax']}")
