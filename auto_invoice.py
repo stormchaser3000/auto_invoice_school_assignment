e25fc4bbd2eebcbc1bbf87729a9be2162bed04b3
Ian Sutter <stormchaser30000@gmail.com>
2018-09-21 21:04:52 -0500

    make the program do invoice calculations and then print the results and needed info on the screen. also satisfy zybooks whitespace requirements

--- auto_invoice.py
+++ auto_invoice.py
@@ -1,7 +1,7 @@
 import math
 
 # print the name of the auto shop
-print('Davy\'s auto shop services')
+print('Dany\'s auto shop services')
 
 # make and print a list of automotive services
 auto_services = {
@@ -14,3 +14,52 @@ print(f"Oil change -- ${auto_services['Oil change']}")
 print(f"Tire rotation -- ${auto_services['Tire rotation']}")
 print(f"Car wash -- ${auto_services['Car wash']}")
 print(f"Car wax -- ${auto_services['Car wax']}")
+
+# print a newline to satisfy whitespace requirements in zybooks
+print()
+
+# get the name of two auto service from the user and read it into a variable (entering a - should make the program not use that variable)
+service_one = input('Select first service:\n')
+service_two = input('Select second service:\n')
+
+# print a newline to satisfy whitespace requrements in zybooks
+print()
+
+# calculate the total price for the invoice
+if service_one in auto_services:
+    price_one = f'${auto_services[service_one]}'
+else:
+    price_one = 'no service'
+if service_two in auto_services:
+    price_two = f'${auto_services[service_two]}'
+else:
+    price_two = 'no service'
+
+if (price_one == 'no service') and (price_two == 'no service'):
+    price_total = 0
+elif price_one == 'no service':
+    price_total = auto_services[service_two]
+elif price_two == 'no service':
+    price_total = auto_services[service_one]
+else:
+    price_total = auto_services[service_one] + auto_services[service_two]
+
+# do some more calculating to make output cleaner
+if price_one == 'no service':
+    service_price_one = price_one
+else:
+    service_price_one = f"{service_one}, {price_one}"
+if price_two == 'no service':
+    service_price_two = price_two
+else:
+    service_price_two = f"{service_two}, {price_two}"
+
+# print the invoice
+print('Davy\'s auto shop invoice')
+# print a newline for zybooks output detecting
+print()
+print(f"Service 1: {service_price_one}")
+print(f"Service 2: {service_price_two}")
+# print another newline for zybooks output detecting
+print()
+print(f"Total: ${price_total}")
